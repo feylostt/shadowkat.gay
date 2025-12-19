9e7bd3cb4a7d68d00e7dc5a111a7ce58e92fec07
shadowkat <earthtoleah@protonmail.com>
2025-12-19 16:07:03 -0600

    moved microblog to main page

--- neko.shadowkat.gay/scripts/nav.js
+++ neko.shadowkat.gay/scripts/nav.js
@@ -2,11 +2,11 @@ document.getElementById('navigation').innerHTML = `
 <div class="right-header">
     <div class="nav-link"><a class="link" href="index.html">home</a></div>
     <div class="nav-link"><a class="link" href="not_found.html">about</a></div>
-    <div class="nav-link"><a class="link" href="microblog.html">microblog</a></div>
+    <div class="nav-link"><a class="link" href="not_found.html">blog</a></div>
     <div class="nav-link" style="border-right: none; --dropdownHeight: 56px;">
-        <a class="dropdown">projects</a>
+        <a class="dropdown">interests</a>
         <div class="dropdown-content">
-            <a class="dropdown-link" href="blog.html">blog</a>
+            <a class="dropdown-link" href="writing.html">writing</a>
             <a class="dropdown-link" href="coding.html">coding</a>
         </div>
     </div>
