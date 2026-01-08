152c3dc4db52c3b2b7e84ed8800faf1d7b711586
shadowkat <earthtoleah@protonmail.com>
2026-01-08 16:07:50 -0600

    backup

--- neko.shadowkat.gay/scripts/nav.js
+++ neko.shadowkat.gay/scripts/nav.js
@@ -2,7 +2,7 @@ document.getElementById('navigation').innerHTML = `
 <div class="right-header">
     <div class="nav-link"><a class="link" href="index.html">home</a></div>
     <div class="nav-link"><a class="link" href="not_found.html">about</a></div>
-    <div class="nav-link"><a class="link" href="not_found.html">blog</a></div>
+    <div class="nav-link"><a class="link" href="https://blog.shadowkat.gay/">blog</a></div>
     <div class="nav-link" style="border-right: none; --dropdownHeight: 56px;">
         <a class="dropdown">interests</a>
         <div class="dropdown-content">
